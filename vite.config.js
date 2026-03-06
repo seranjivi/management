dcfde63ed37ce9b8ae15ff571b56843acc54af06
seranjivi <seranjivi.k@cardyai.com>
2026-03-06 15:20:04 +0530

    second com2345mit

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,12 @@
-import { defineConfig } from "vite";import react from "@vitejs/plugin-react";export default defineConfig({ plugins: [react()], server: { port: 3000 }, build: { rollupOptions: { input: "management-pages.jsx" } } });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: { port: 3000 },
+  build: {
+    rollupOptions: {
+      input: "index.html"
+    }
+  }
+});
